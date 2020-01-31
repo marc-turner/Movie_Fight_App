713db229c04678e2b32bb85b8dfe295263119ec6
Marc Turner <marc.t1967@gmail.com>
2020-01-31 12:49:25 -0500

    add input functionality

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hi there!');
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '77cd1df5',
+            s: searchTerm
+        }
+    });
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+let timeoutId;
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 1000);
+};
+
+input.addEventListener('input', onInput);
