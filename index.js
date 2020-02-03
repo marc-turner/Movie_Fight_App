d13023e3b1297cceee39230847c0892b9962a442
Marc Turner <marc.t1967@gmail.com>
2020-02-03 16:56:09 -0500

    add html generation

--- index.js
+++ index.js
@@ -11,19 +11,35 @@ const fetchData = async searchTerm => {
     return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+<label><b>Search For a Movie</b></label>
+<input type="text" class="input"/>
+<div class ="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+</div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
-    // console.log(movies);
+
+    dropdown.classList.add('is-active');
     for (let movie of movies) {
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <img src="${movie.Poster}/>"
-        <h1>${movie.Title}</h1>
+        const option = document.createElement('a');
+
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+        <img src="${movie.Poster}"/>
+        ${movie.Title}
         `;
 
-        const target = document.querySelector('#target').appendChild(div);
+        resultsWrapper.appendChild(option);
     }
 };
 
